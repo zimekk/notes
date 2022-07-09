5777fdf8cbc9235ed49d1f522ec652472d4b86eb
Marcin Makarewicz <marcin@makarewicz.eu>
2022-07-09 02:29:50 +0200

    test: add mocks

---
+++ jest.setup.js
@@ -0,0 +1,14 @@
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
+jest.mock('react-native-reanimated', () =>
+  require('react-native-reanimated/mock'),
+);
+jest.mock('@react-native-community/push-notification-ios', () => ({
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  requestPermissions: jest.fn(() => Promise.resolve({})),
+  // configure: jest.fn()
+}));
