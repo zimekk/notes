c4e1b2a14c1a49383246e67d01acd69432ca724b
Marcin Makarewicz <marcin@makarewicz.eu>
2022-08-09 23:39:07 +0200

    feat: use react-native-svg

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
-import {Button, StatusBar, Text, useColorScheme} from 'react-native';
+import {Button, StatusBar, Text, useColorScheme, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import Svg, {Path} from 'react-native-svg';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import styled from 'styled-components';
+
+const StyledView = styled(View)`
+  padding: 20px;
+  flex-direction: column;
+  align-items: center;
+  background-color: #355070;
+`;
+
+const StyledText = styled(Text)`
+  color: #eaac8b;
+`;
+
+function Logo() {
+  return (
+    <Svg viewBox="0 0 8 8" width="100" height="100">
+      <Path d="M0 0v8h4V3c0-3-2-3-4-3" fill="#6d597a" />
+      <Path d="M4 0v8h4V3c0-3-2-3-4-3" fill="#b56576" />
+    </Svg>
+  );
+}
 
 export default function HomeScreen({navigation}) {
   const isDarkMode = useColorScheme() === 'dark';
@@ -14,6 +36,10 @@ export default function HomeScreen({navigation}) {
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <Text>{JSON.stringify({isDarkMode}, null, 2)}</Text>
+      <StyledView>
+        <Logo />
+        <StyledText>Logo</StyledText>
+      </StyledView>
       <Button
         title="AnimatedScreen"
         onPress={() => navigation.navigate('AnimatedScreen')}
